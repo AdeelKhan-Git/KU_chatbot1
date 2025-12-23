cf5d4ee188496fec87319608b961222ff2e4a82a
kid1356 <engr.adeelkhan2002@gmail.com>
2025-12-23 21:31:03 +0500

    remove ollama embedding and added Gemini embeddng

--- Backend/Chatbot/chatapi/embedding.py
+++ Backend/Chatbot/chatapi/embedding.py
@@ -1,4 +1,17 @@
-from langchain_ollama import OllamaEmbeddings
-# from langchain.embeddings import OpenAIEmbeddings
 
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
+from phi.embedder.google import GeminiEmbedder
+import os
+
+
+
+gemini_api_key = os.environ.get("GOOGLE_API_KEY")
+
+
+
+
+gemini_embedder = GeminiEmbedder(
+    model="models/text-embedding-004",
+    api_key=gemini_api_key
+)
+
+
