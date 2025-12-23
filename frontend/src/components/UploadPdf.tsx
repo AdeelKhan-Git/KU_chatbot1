6a3eacbe8a908d93c05c5c0df9e4ecb75d337469
kid1356 <engr.adeelkhan2002@gmail.com>
2025-12-23 21:50:53 +0500

    added loader and some adjustments

--- frontend/src/components/UploadPdf.tsx
+++ frontend/src/components/UploadPdf.tsx
@@ -1,28 +1,31 @@
 import { shrinkText } from "@/lib/utils";
-import { CloudUpload } from "lucide-react";
+import { CloudUpload, Loader2 } from "lucide-react";
 import React, { useCallback, useState } from "react";
 import toast from "react-hot-toast";
 
 interface UploadPdfProps {
   onUpload?: (file: File) => void;
   file?: File | null;
+  loading: boolean;
+  pdfFile?: File | null;
+  handleUploadFile: () => Promise<void>;
 }
 
-const UploadPdf: React.FC<UploadPdfProps> = ({ onUpload, file }) => {
+const UploadPdf: React.FC<UploadPdfProps> = ({ onUpload, file, loading, handleUploadFile, pdfFile }) => {
   const [isDragOver, setIsDragOver] = useState(false);
 
   const handleFileSelect = (file: File) => {
-    const maxSize = 10 * 1024 * 1024;
+    // const maxSize = 10 * 1024 * 1024;
 
     if (!file.name.toLowerCase().endsWith(".pdf")) {
       toast.error("Only PDF files are allowed");
       return;
     }
 
-    if (file.size > maxSize) {
-      toast.error("File size must be less than 10MB");
-      return;
-    }
+    // if (file.size > maxSize) {
+    //   toast.error("File size must be less than 10MB");
+    //   return;
+    // }
 
     onUpload && onUpload(file);
   };
@@ -59,54 +62,73 @@ const UploadPdf: React.FC<UploadPdfProps> = ({ onUpload, file }) => {
         onDrop={handleDrop}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
-        className={`border-2 gap-4 sm:justify-between flex-wrap sm:flex-nowrap items-center border-dashed rounded-md w-full p-36 text-center transition-all duration-200 ${
-          isDragOver
-            ? "bg-green-500/20 backdrop-blur-md border border-green-500/30"
-            : file
+        className={`flex flex-col border-2 gap-4 sm:justify-center flex-wrap sm:flex-nowrap items-center border-dashed rounded-md w-full p-36 text-center transition-all duration-200 ${isDragOver
+          ? "bg-green-500/20 backdrop-blur-md border border-green-500/30"
+          : file
             ? "bg-blue-100/10 backdrop-blur-sm border border-white/20"
             : "border-warning-150"
-        }`}
+          }`}
       >
-        <div className="gap-4">
-          <div className="flex m-auto justify-center">
-            <CloudUpload className="w-10 h-10 " />
-          </div>
 
-          {file ? (
-            <div className="flex text-center gap-2 flex-col">
-              <p className="text-lg font-semibold text-primary-300 whitespace-pre-line">
-                File Selected: {shrinkText(file.name, 20)}
-              </p>
-              <p className="text-sm pb-1 text-primary-300">
+        {loading ? (
+          <div className="size-14 animate-spin text-white">
+            <Loader2 className="size-full" />
+          </div>
+        ) : (
+          <>
+            <div className="gap-4">
+              <div className="flex m-auto justify-center">
+                <CloudUpload className="w-10 h-10 " />
+              </div>
+
+              {file ? (
+                <div className="flex text-center gap-2 flex-col">
+                  <p className="text-lg font-semibold text-primary-300 whitespace-pre-line">
+                    File Selected: {shrinkText(file.name, 20)}
+                  </p>
+                  {/* <p className="text-sm pb-1 text-primary-300">
                 Size: {(file.size / 1024 / 1024).toFixed(2)} MB
-              </p>
+              </p> */}
+                </div>
+              ) : (
+                <div className="text-center mx-auto gap-2 sm:max-w-72 ">
+                  <p className="text-sm font-semibold text-primary-300 whitespace-pre-line">
+                    Supported formats: PDF
+                  </p>
+                  <p className="text-sm pb-1 text-primary-300">
+                    Drag and Drop a PDF file here or
+                  </p>
+                </div>
+              )}
             </div>
-          ) : (
-            <div className="text-center mx-auto gap-2 sm:max-w-72 ">
-              <p className="text-sm font-semibold text-primary-300 whitespace-pre-line">
-                Supported formats: PDF | Max size: 10MB
-              </p>
-              <p className="text-sm pb-1 text-primary-300">
-                Drag and Drop a PDF file here or
-              </p>
+
+            <div>
+              <input
+                type="file"
+                id="file-input"
+                className="hidden"
+                accept=".pdf"
+                onChange={handleFileInputChange}
+              />
+              <label
+                htmlFor="file-input"
+                className="inline-block px-8 py-2 bg-white border border-warning-150 text-black rounded-md font-semibold cursor-pointer hover:bg-neutral-50 hover:border-neutral-400 transition-all duration-200"
+              >
+                Choose File
+              </label>
             </div>
-          )}
-        </div>
-        <div>
-          <input
-            type="file"
-            id="file-input"
-            className="hidden"
-            accept=".pdf"
-            onChange={handleFileInputChange}
-          />
-          <label
-            htmlFor="file-input"
-            className="inline-block px-8 py-2 bg-white border border-warning-150 text-black rounded-md font-semibold cursor-pointer hover:bg-neutral-50 hover:border-neutral-400 transition-all duration-200"
-          >
-            Choose File
-          </label>
-        </div>
+
+            {pdfFile && <button
+              disabled={!pdfFile || loading}
+              className="inline-block px-8 py-2 bg-white border border-warning-150 text-black rounded-md font-semibold cursor-pointer"
+              onClick={handleUploadFile}
+            >
+              Upload Now
+            </button>}
+          </>
+        )}
+
+
       </div>
     </div>
   );
