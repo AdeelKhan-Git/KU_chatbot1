1db28389c22ca11b116ae40aba8e0236f0deffb1
kid1356 <engr.adeelkhan2002@gmail.com>
2025-12-23 21:53:24 +0500

    some adjustments

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -6,14 +6,14 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export const shrinkText = (text: string, maxLength: number = 20): string => {
-  if (text.length <= maxLength) return text;
+  if (text?.length <= maxLength) return text;
   const ellipsis = "...";
   const charsToShow = maxLength - ellipsis.length;
   const frontChars = Math.ceil(charsToShow / 2);
   const backChars = Math.floor(charsToShow / 2);
 
-  const start = text.slice(0, frontChars);
-  const end = text.slice(-backChars);
+  const start = text?.slice(0, frontChars);
+  const end = text?.slice(-backChars);
 
   return `${start}${ellipsis}${end}`;
 };
