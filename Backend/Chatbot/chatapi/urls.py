310763b393f60729ee7df8c4c32374b998ffea65
kid1356 <engr.adeelkhan2002@gmail.com>
2025-12-23 21:36:25 +0500

    new path added

--- Backend/Chatbot/chatapi/urls.py
+++ Backend/Chatbot/chatapi/urls.py
@@ -1,10 +1,11 @@
-from .views import ChatBotAPIView,UploadFileView,UploadedDataListView
+from .views import ChatBotAPIView,UploadFileView,UploadedDataListView,GetChatDataView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('chat/',ChatBotAPIView.as_view(), name = 'chatbotresponse'), 
     path('upload_file/',UploadFileView.as_view(), name = 'uploadfile'),
+    path('chat-data/',GetChatDataView.as_view(), name = 'chatdata'),
     path('file_records',UploadedDataListView.as_view(), name= 'record_list'), 
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),  
 ]
