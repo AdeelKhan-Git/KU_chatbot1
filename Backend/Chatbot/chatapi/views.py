d529351668b9c041db4c2fe9cedeab42a1381e68
kid1356 <engr.adeelkhan2002@gmail.com>
2026-01-01 18:52:07 +0500

    optimize uploadfile view

--- Backend/Chatbot/chatapi/views.py
+++ Backend/Chatbot/chatapi/views.py
@@ -1,12 +1,17 @@
 from datetime import timedelta
+from django.db import transaction
 from django.utils import timezone
 from rest_framework.views import APIView
 from rest_framework import status,permissions
-from rest_framework.parsers import MultiPartParser
+from rest_framework.parsers import MultiPartParser,FormParser
 from rest_framework.response import Response
 from .models import UploadRecord,ChatMessage
+from .embedding import openai_embedder
+from phi.knowledge.pdf import PDFKnowledgeBase
+from phi.vectordb.pgvector import PgVector2
 from .serializer import ChatMessageSerializer,UploadSerializer
-from .utils import ask_phi,agent
+from .utils import ask_phi,SafePDFReader
+from phi.document.chunking.document import DocumentChunking
 
 
 
@@ -22,36 +27,62 @@ class ChatBotAPIView(APIView):
         if not prompt:
             return Response({'error':'prompt is required'},status=status.HTTP_400_BAD_REQUEST)
         
-        full_response = ""
-        for chunk in ask_phi(request.user, prompt):
-            full_response += chunk
+        try:
+            full_response = ""
+            for chunk in ask_phi(request.user, prompt):
+                chunk = chunk.replace("<br>", "\n")
+                full_response += chunk
 
-        return Response({"response": full_response.strip()})
+            return Response({"response": full_response.strip()})
+        except Exception as e:
+            return Response({'error': str(e)},status=status.HTTP_400_BAD_REQUEST)
         
                     
 class UploadFileView(APIView):
     permission_classes = [permissions.IsAdminUser]
-    parser_classes = [MultiPartParser]
+    parser_classes = [MultiPartParser,FormParser]
 
     def post(self, request):
         file = request.FILES.get("file")
 
         if not file:
-            return Response({"error": "No file uploaded"}, status=400)
+            return Response({"error": "No file uploaded"}, status=status.HTTP_400_BAD_REQUEST)
 
         if not file.name.lower().endswith(".pdf"):
-            return Response({"error": "Only PDF files allowed"}, status=400)
+            return Response({"error": "Only PDF files allowed"}, status=status.HTTP_400_BAD_REQUEST)
 
-        # Save file
-        pdf = UploadRecord.objects.create(
-            file=file,
-            name=file.name,
-            uploaded_by=request.user,
-        )
+        try:
+            with transaction.atomic():
+                pdf = UploadRecord(
+                            file=file,
+                            name=file.name,
+                            uploaded_by=request.user,
+                        )
 
-        # Re-index knowledge base
-        agent.knowledge.load(recreate=False)
 
+        
+                pdf_knowledge_base = PDFKnowledgeBase(
+                    path=pdf.file.path,
+                    vector_db=PgVector2(
+                        collection="UoK_Data",
+                        db_url="postgresql+psycopg://ai:ai@localhost:5532/ai",
+                        embedder=openai_embedder
+                        
+                    ),
+                    reader=SafePDFReader(
+                        chunk=True,
+                        chunking_strategy = DocumentChunking(chunk_size=5000, overlap=150)),
+                    )
+                
+    
+                 # Re-index knowledge base
+                pdf_knowledge_base.load()
+
+                pdf.save()
+        
+        except Exception as e:
+            return Response({'error': f"Failed to process PDF: {str(e)}"},status=status.HTTP_400_BAD_REQUEST)
+        
         return Response(
             {
                 "message": "PDF uploaded and indexed successfully",
