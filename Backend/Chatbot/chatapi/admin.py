d497b31f8e74bd44441c6ee9f69edc7a6c4fc33b
kid1356 <engr.adeelkhan2002@gmail.com>
2026-01-01 18:47:12 +0500

    removed knowledge base

--- Backend/Chatbot/chatapi/admin.py
+++ Backend/Chatbot/chatapi/admin.py
@@ -1,21 +1,7 @@
 from django.contrib import admin
-from .models import KnowledgeBase,UploadRecord,ChatMessage
-# from django.contrib.auth.models import User
+from .models import UploadRecord,ChatMessage
 
 # Register your models here.
-
-@admin.register(KnowledgeBase)
-class AdminKnowledgeBase(admin.ModelAdmin):
-    list_display = ['id','file_name','page','short_content']
-
-    def short_content(self, obj):
-        obj = obj.content.split()
-        short_txt = ''.join(obj[:20])
-        if len(obj)>20:
-            short_txt += '...'
-        return short_txt
-
-    short_content.short_description = "Content(first 20 words)" 
 
 @admin.register(UploadRecord)
 class AdminUpload(admin.ModelAdmin):
