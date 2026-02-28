11e0f1b8c8a7682435d6af3af142ccf037b9e816
kid1356 <engr.adeelkhan2002@gmail.com>
2026-02-28 23:00:15 +0500

    optimize the code

--- Backend/Chatbot/chatapi/task.py
+++ Backend/Chatbot/chatapi/task.py
@@ -1,27 +1,27 @@
 from celery import shared_task
 from .models import UploadRecord
-from phi.knowledge.pdf import PDFKnowledgeBase
-from phi.vectordb.pgvector import PgVector2
 from .embedding import openai_embedder
-from .utils import SafePDFReader,posgre_url
-from phi.document.chunking.document import DocumentChunking
+from .utils import get_pdf_knowledge_base
+from phi.document.chunking.semantic import SemanticChunking
 
 @shared_task
 def process_pdf(upload_id):
     pdf = UploadRecord.objects.get(id=upload_id)
 
+    try:
+        pdf.status = 'processing'
+        pdf.save(update_fields=['status'])
+       
+      
+        pdf_knowledge_base = get_pdf_knowledge_base(pdf.file.path)
+            
+        
+        pdf_knowledge_base.load(recreate=False)
 
-    pdf_knowledge_base = PDFKnowledgeBase(
-        path=pdf.file.path,
-        vector_db=PgVector2(
-        collection="UoK_Data",
-        db_url=posgre_url,
-        embedder=openai_embedder
-                        
-        ),
-        reader=SafePDFReader(
-        chunk=True,
-        chunking_strategy = DocumentChunking(chunk_size=800, overlap=150)),
-        )
-    
-    pdf_knowledge_base.load(recreate=False)
+        pdf.status = 'completed'
+        pdf.save(update_fields=['status'])
+        
+    except Exception as e:
+        pdf.status = 'failed'
+        pdf.save(update_fields=['status'])
+        raise e
