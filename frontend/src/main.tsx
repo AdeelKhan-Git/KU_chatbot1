90918c7c1bde187ebe7a58244e580fa18bfc5df9
kid1356 <engr.adeelkhan2002@gmail.com>
2026-03-01 00:14:30 +0500

    bug fixed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Toaster } from "react-hot-toast";
 import { BrowserRouter } from "react-router-dom";
