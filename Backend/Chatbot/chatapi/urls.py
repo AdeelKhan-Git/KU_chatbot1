6778992cae94e4a50d039d054a5674f03047d9c8
kid1356 <engr.adeelkhan2002@gmail.com>
2026-02-28 23:51:42 +0500

    add new endpoint upload status

--- Backend/Chatbot/chatapi/urls.py
+++ Backend/Chatbot/chatapi/urls.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.views import TokenVerifyView
 
 urlpatterns = [
     path('chat/',ChatBotAPIView.as_view(), name = 'chatbotresponse'), 
-    path('upload_file/',UploadFileView.as_view(), name = 'uploadfile'),
-    path('upload_status/<int:pdf_id>/',UploadStatusView.as_view(), name = 'uploadstatus'),
+    path('upload-file/',UploadFileView.as_view(), name = 'uploadfile'),
+    path('upload-status/<int:pdf_id>/',UploadStatusView.as_view(), name = 'uploadstatus'),
     path('chat-data/',GetChatDataView.as_view(), name = 'chatdata'),
-    path('file_records',UploadedDataListView.as_view(), name= 'record_list'), 
+    path('file-records',UploadedDataListView.as_view(), name= 'record_list'), 
     path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),  
 ]
