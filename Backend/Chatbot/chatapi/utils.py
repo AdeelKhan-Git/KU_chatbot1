0decaba34780515a33feae2fe0094d747ac1fdef
kid1356 <engr.adeelkhan2002@gmail.com>
2026-02-28 23:54:25 +0500

    optimize the code and add semantic chuncker

--- Backend/Chatbot/chatapi/utils.py
+++ Backend/Chatbot/chatapi/utils.py
@@ -15,8 +15,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 PDF_DIR = os.path.join(BASE_DIR, "media", "pdfs")
 
-# posgre_url = os.getenv("DATABASE_URL")
-posgre_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+posgre_url = os.getenv("DATABASE_URL")
+# posgre_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
 open_api_key = os.environ.get("OPENAI_API_KEY")
 
