88adf42deea4a12ad2d6865cc0f39bfb41310878
kid1356 <engr.adeelkhan2002@gmail.com>
2026-03-01 00:01:58 +0500

    ==

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,12 +9,12 @@ import "./index.css";
 import "./styles/app.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+  <>
     <BrowserRouter>
       <GoogleOAuthProvider clientId={"292867197714-hqll2rb5nvhdp22l614rjk2fscmab42d.apps.googleusercontent.com"}>
         <Toaster position="top-right" />
         <App />
       </GoogleOAuthProvider>
     </BrowserRouter>
-  </StrictMode>
+  </>
 );
