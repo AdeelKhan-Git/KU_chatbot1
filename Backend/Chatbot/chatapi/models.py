847f8c9228b2093ff1ec23ad9f985c5012bd2ef2
Muhammad Adnan <engr.adeelkhan2002@gmail.com>
2026-04-25 20:33:53 +0500

    add session id on ChatMessage model

--- Backend/Chatbot/chatapi/models.py
+++ Backend/Chatbot/chatapi/models.py
@@ -29,6 +29,7 @@ class ChatMessage(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='chat_messages')
     role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    session_id = models.CharField(max_length=255, null= True, blank=True, db_index=True)
     content = models.TextField()
     timestemp = models.DateTimeField(auto_now_add=True)
-    
+        
